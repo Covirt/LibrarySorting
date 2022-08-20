3e4616a54f7e713d05863bffc19e666ef3cb2ce2
Covirt <vg.szwarcberg@gmail.com>
2022-08-20 16:18:24 +0100

    Slightly wrong vector creation

--- main.cpp
+++ main.cpp
@@ -15,20 +15,23 @@ struct Book{
 int main(){
     fstream dataFile;
     string txt;
+    vector<vector<string>> BigData;
+    vector<string> tempLine;
 
     dataFile.open("Data.csv",fstream::in);
 
     if (dataFile.fail()){cout << "Input file opening failed. \n";}
     else{
+        int i = 0;
         while (getline(dataFile,txt,',')){
-            cout << txt << endl;
+            tempLine.push_back(txt);
+            cout << i << "|" <<txt << endl;
+            if (txt.find("\n") != string::npos){
+                i++;
+                BigData.push_back(tempLine);
+                tempLine.clear();
+            }
         }
     }
-
-
-
-
-    cout << "Hello World" << endl;
-    cout << "This is a new feature!" << endl;
     return 0;
 }
