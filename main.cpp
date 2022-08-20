89cabc571814929298efc4577af0ef128e20fdd1
Covirt <vg.szwarcberg@gmail.com>
2022-08-20 15:35:49 +0100

    Implementation of "struct Book" and file reading

--- main.cpp
+++ main.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
 
 using namespace std;
 
+struct Book{
+    string Title;
+    string Author;
+    int publishDate;
+    char genderAuthor;
+};
+
+
 int main(){
+    fstream dataFile;
+    string txt;
+
+    dataFile.open("Data.csv",fstream::in);
+
+    if (dataFile.fail()){cout << "Input file opening failed. \n";}
+    else{
+        while (getline(dataFile,txt,',')){
+            cout << txt << endl;
+        }
+    }
+
+
+
+
     cout << "Hello World" << endl;
     cout << "This is a new feature!" << endl;
     return 0;
