64df1c18f35b85dbb8c418c6d04a720c6c284e2f
Covirt <vg.szwarcberg@gmail.com>
2022-08-21 14:37:38 +0100

    Cleaning Up

--- main.cpp
+++ main.cpp
@@ -16,21 +16,30 @@ int main(){
     fstream dataFile;
     string txt;
     vector<vector<string>> BigData;
-    vector<string> tempLine;
-
+    
     dataFile.open("Data.csv",fstream::in);
 
     if (dataFile.fail()){cout << "Input file opening failed. \n";}
     else{
         int i = 0;
-        while (getline(dataFile,txt,',')){
-            tempLine.push_back(txt);
-            cout << i << "|" <<txt << endl;
-            if (txt.find("\n") != string::npos){
-                i++;
-                BigData.push_back(tempLine);
-                tempLine.clear();
+        vector<string> tempLine;
+        
+        while (getline(dataFile,txt)){
+            string tempString;
+
+            while (txt.length() > 0){
+                if (txt[0] != ','){
+                    tempString.push_back(txt[0]);
+                    txt.erase(0,1);
+                }
+                else{
+                    txt.erase(0,1);
+                    tempLine.push_back(tempString);
+                    tempString.clear();
+                }
             }
+            BigData.push_back(tempLine);
+            tempLine.clear();
         }
     }
     return 0;
